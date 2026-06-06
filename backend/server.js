99070df9cef66ac87e1a78d673cd07d0ce231627
komal200515 <komalmahawar637@gmail.com>
2026-06-06 14:24:40 +0530

    update url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ require('dotenv').config();
 
 const allowedOrigins = [
   'http://localhost:5500',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  'https://eventmedia-komal.vercel.app',
 ];
 
 const app        = express();
